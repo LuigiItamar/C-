dedae063afe487e52bf819cfee83782966361b14
luigi-apis3 <luigi.fidencio@apis3.com>
2024-08-27 09:53:26 -0300

    base

---
+++ HelloWord/HelloWord/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace HelloWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var boas_vindas = "192";
+            var name = "203";
+            var soma = name + boas_vindas;
+              
+            Console.WriteLine(boas_vindas);
+            Console.WriteLine(name);
+            Console.WriteLine(soma);
+            Console.ReadLine();
+
+        }
+    }
+}
